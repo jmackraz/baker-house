f205c868eebd44607a27f39f1c952cfa99084cec
Jim Mackraz <jim@trickybit.com>
2018-03-26 23:32:33 -0700

    Added schema validation and last-modified-time recording

--- hub/hub/views.py
+++ hub/hub/views.py
@@ -1,36 +1,24 @@
 """ Cornice services.
 """
 import logging
+import datetime
+
+import colander
 
 from cornice import Service
 from cornice.resource import resource
+from cornice.validators import colander_body_validator
+
+from cornice.validators import extract_cstruct
 
 log = logging.getLogger(__name__)
 
-
-log.debug("service starting")
-
 hello = Service(name='hello', path='/', description="Simplest app")
-
-_VALUE = {'Hello': 'World'}
-
 
 @hello.get()
 def get_info(request):
     """Returns Hello in JSON."""
-    return _VALUE
-
-@hello.put()
-def put_info(request):
-    """messing around"""
-    global _VALUE
-    try:
-        # json_body is JSON-decoded variant of the request body
-        log.debug("_VALUE before %s", _VALUE)
-        _VALUE = request.json_body
-    except ValueError:
-        return False
-    return True
+    return  {'message': 'Baker Home REST API'}
 
 
 devices = Service(name='devices', path='/devices', description="Discovery")
@@ -50,18 +38,47 @@ def get_receiver(request):
             'control': {'input': 'directv', 'volume': 40}
             }
 
-#receiver_ctl = Service(name='receiver_ctl', path='/receiver/control', description="The one and only receiver")
 
 _CONTROL={'input': 'directv', 'volume': 40}
 
-@resource(path='/receiver/control')
+# don't blow out the volume
+VOLUME_LEVEL_CAP=50
+
+# ZZZ: use this in a last-modified response header,
+# so iot can selectively update the device shadow
+#
+_last_modify_time = datetime.utc()
+def _update_last_modify_time():
+    global _last_modify_time
+    _last_modify_time = datetime.utc()
+
+
+def _update_receiver( knobs ):
+    """adjust the receiver by remote control"""
+    if 'input' in knobs:
+        log.info("set receiver input to: %s", knobs['input'])
+
+    if 'volume' in knobs:
+        capped_level = min(knobs['volume'], VOLUME_LEVEL_CAP)
+        log.info("set receiver volume to: %s (capped, requested: %s)", capped_level, knobs['volume'])
+        knobs['volume'] = capped_level
+
+    global _CONTROL
+
+    if len(knobs) > 0:
+        _update_last_modify_time()
+        _CONTROL.update(knobs)
+
+
+class ControlSchema(colander.MappingSchema):
+    input_node = colander.SchemaNode(colander.String(), name="input", missing=colander.drop)
+    volume = colander.SchemaNode(colander.Int(), missing=colander.drop)
+
+@resource(accept="text/json", path='/receiver/control', schema=ControlSchema(), validators=(colander_body_validator,) )
 class ReceiverControl:
     def __init__(self, request, context = None):
-        log.debug("ReceiverControl init()")
+        #log.debug("ReceiverControl init()")
         self.request = request
-
-    def __acl__(self):
-        return [(Allow, Everyone, 'everything')]
 
     def get(self):
         log.debug("get_receiver_ctl")
@@ -70,8 +87,9 @@ class ReceiverControl:
     def put(self):
         """control recevier"""
         log.debug("put_receiver_ctl")
-        global _CONTROL
-        control_delta = self.request.json_body
-        _CONTROL.update(control_delta)
+
+        knobs = self.request.validated
+        _update_receiver(knobs)
+
         return _CONTROL
 
