8f61c74e37b60f7d285af3d579556647e4062659
Jim Mackraz <jim@trickybit.com>
2018-03-29 20:18:01 -0700

    Created test script that uses AWS CLI to poke the Thing in the cloud.

--- scripts/start_iot.sh
+++
@@ -1,6 +0,0 @@
-# start iot handler
-#
-
-
-DIR="$(dirname $0)"
-python $DIR/../src/house_iot.py -e $BAKERHOUSE_ENDPOINT -r $BAKERHOUSE_ROOTCERT_FILE -c $BAKERHOUSE_MYCERT_FILE -k $BAKERHOUSE_PRIVATEKEY_FILE --mode subscribe
