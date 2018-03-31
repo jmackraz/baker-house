9883126271aa7df00ea4da7db2fa18a6f03a9705
Jim Mackraz <jim@trickybit.com>
2018-03-30 23:49:45 -0700

    set up both a pubsub and shadow connection; it times out. About to simplify.

--- src/house_iot.py
+++ src/house_iot.py
@@ -7,8 +7,7 @@ import argparse
 import json
 from os import environ
 
-#from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient, AWSIoTMQTTShadowClient
 
 
 # config from environment
@@ -52,17 +51,24 @@ log.addHandler(streamHandler)
 def subscribeCallback(client, userdata, message):
     parsed = json.loads(message.payload)
     pretty_json = json.dumps(parsed, indent=2, sort_keys=True)
-    log.info("message received from topic %s: %s", message.topic, pretty_json)
+    #log.info("message received from topic %s: %s", message.topic, pretty_json)
+    log.info("message received from topic %s", message.topic)
 
 
 shadow_client = None
 if useWebsocket:
-    #mqtt_client = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    mqtt_client = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    mqtt_client.configureEndpoint(host, 443)
+    mqtt_client.configureCredentials(rootCAPath)
+
     shadow_client = AWSIoTMQTTShadowClient(clientId, useWebsocket=True)
     shadow_client.configureEndpoint(host, 443)
     shadow_client.configureCredentials(rootCAPath)
 else:
-    #mqtt_client = AWSIoTMQTTClient(clientId)
+    mqtt_client = AWSIoTMQTTClient(clientId)
+    mqtt_client.configureEndpoint(host, 8883)
+    mqtt_client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
     shadow_client = AWSIoTMQTTShadowClient(clientId)
     shadow_client.configureEndpoint(host, 8883)
     shadow_client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
@@ -71,12 +77,16 @@ else:
 shadow_client.configureAutoReconnectBackoffTime(1, 32, 20)
 shadow_client.configureConnectDisconnectTimeout(10)  # 10 sec
 shadow_client.configureMQTTOperationTimeout(5)  # 5 sec
-
-#pubish stuff
-#shadow_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-#shadow_client.configureDrainingFrequency(2)  # Draining: 2 Hz
-
 shadow_client.connect()
+
+# pubsub stuff
+mqtt_client.configureAutoReconnectBackoffTime(1, 32, 20)
+mqtt_client.configureConnectDisconnectTimeout(10)  # 10 sec
+mqtt_client.configureMQTTOperationTimeout(5)  # 5 sec
+mqtt_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
+mqtt_client.connect()
+
 
 class ShadowCallback:
     def __init__(self):
@@ -97,16 +107,16 @@ shadow_callback = ShadowCallback()
 shadow_handler.shadowRegisterDeltaCallback(shadow_callback.delta)
 
 
-mqtt_client = shadow_client.getMQTTConnection()
-mqtt_client.subscribe(topic, 1, subscribeCallback)
+#mqtt_client = shadow_client.getMQTTConnection()
 
+mqtt_client.subscribe(topic, 1, subscribeCallback)
 
 if shadowTopicSubscribe:
     for topic in device_shadow_topics:
         mqtt_client.subscribe(topic.format(thingName), 1, subscribeCallback)
         
 
-log.info("connection made")
+log.info("connected")
 
 # poll the source of truth
 while True:
