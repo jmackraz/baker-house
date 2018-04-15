f48893b5da79b53b2ae9712cb77612ae12205e38
Jim Mackraz <jim@trickybit.com>
2018-04-14 18:22:05 -0700

    workaround "--no-dry-run" bug in uploading command, and use new handler code. Alexa Test Works.

--- scripts/update_lambda.sh
+++ scripts/update_lambda.sh
@@ -2,17 +2,22 @@
 #!/bin/bash
 
 DIR="$(dirname $0)"
-python_file="$DIR/../src/house_lambda.py"
+python_file="$DIR/../src/new_house_lambda.py"
+#python_file="$DIR/../src/house_lambda.py"
 zip_file="/tmp/tmp_lambda.zip"
 lambda_name=$BAKERHOUSE_LAMBDA
 
-#dry_run="--no-dry-run"
-dry_run="--dry-run"
+# BUG! "--no-dry-run" does not work!
+dry_run=""
+#dry_run="--dry-run"
 
 echo publishing $python_file
 
 # -j means "junk (omit) the path
 zip -j $zip_file $python_file
 
-aws lambda update-function-code --function-name $lambda_name --zip-file  fileb://$zip_file --no-publish $dry_run
+#aws lambda update-function-code --function-name $lambda_name --zip-file  fileb://$zip_file --no-publish $dry_run
+cmd="aws lambda update-function-code --function-name $lambda_name --zip-file  fileb://$zip_file --no-publish $dry_run"
+echo CMD: $cmd
+$cmd
 
